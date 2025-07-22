acd0bc0643e1dda0fcd5c6cd0624f95fadefdfb3
Michel Romero <studentenherz@gmail.com>
2025-07-22 15:39:33 -0300

    Add `@vqueue/sdk` mapping for jest

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   verbose: true,
   moduleNameMapper: {
+    '^@vqueue/sdk(.*)$': '<rootDir>/src$1',
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
